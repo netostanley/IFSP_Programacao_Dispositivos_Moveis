9d3bdfbfd17b614021765ec5ce15cae16d7a4abe
netostanley <105943945+netostanley@users.noreply.github.com>
2024-10-07 19:30:51 -0300

    Add files via upload

---
+++ PDMI6- Trabalho Prático 01/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnOKClicked(object sender, EventArgs e)
+        {
+            string id = entryID.Text;
+            string senha = entrySenha.Text;
+
+            if (id == "admin" && senha == "senha@dmin")
+            {
+                DisplayAlert("Login", "Logado com sucesso!", "OK");
+            }
+            else
+            {
+                DisplayAlert("Login", "Login não autorizado!", "OK");
+            }
+        }
+
+        private void OnLimparClicked(object sender, EventArgs e)
+        {
+            entryID.Text = string.Empty;
+            entrySenha.Text = string.Empty;
+            entryID.Focus();
+        }
+
+        private void OnCreditoClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Créditos", "Autores: Eric Nahas, Armando Righi de Souza", "OK");
+        }
+    }
+
+}
